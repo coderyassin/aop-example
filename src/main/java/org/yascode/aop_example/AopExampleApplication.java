b2245e7a724034904e294f256327e869bdcfe86f
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-07-20 10:35:22 +0100

    initial commit

---
+++ src/main/java/org/yascode/aop_example/AopExampleApplication.java
@@ -0,0 +1,13 @@
+package org.yascode.aop_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopExampleApplication.class, args);
+	}
+
+}
